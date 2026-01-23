e2a8174a4f5e58608d627c73b7b49850b979ab0e
Jeanett Napier <129127168+Jonatid@users.noreply.github.com>
2026-01-23 13:18:36 -0500

    only ever use the API key

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY || process.env.SMTP_PASS);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEmail({ to, subject, html }) {
   if (!process.env.EMAIL_FROM) {
